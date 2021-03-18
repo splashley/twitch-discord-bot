04955e09edc3ae48f23186a385f8b4af0434f3a1
splashley <ashleyjlhynes@gmail.com>
2021-03-17 20:22:51 -0400

    fixed bug

--- src/commands/wordgame.js
+++ src/commands/wordgame.js
@@ -7,17 +7,11 @@ module.exports = {
     let originalWord = "";
     let changedWord = "";
 
-    const gameStatusTrue = () => {
-      client.say(
-        channel,
-        `There is a game in progress already. The scrambled word is ${currentGameStatus.scrambled}. Good luck!`
-      );
-    };
-
     const checkGameStatus = async () => {
       const { active, scrambled } = await firebase.getGameStatus();
-      if (active === "true") {
+      if (active === true) {
         client.say(
+          channel,
           `There is already a game going on. The scrambled word is ${scrambled}. Type !guess word to guess your answer. Good luck!`
         );
       } else {
