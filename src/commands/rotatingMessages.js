a51778b3d4c699256d9e13a0c43831e0893815ff
splashley <ashleyjlhynes@gmail.com>
2021-03-31 15:26:45 -0400

    updates

--- src/commands/rotatingMessages.js
+++ src/commands/rotatingMessages.js
@@ -3,11 +3,13 @@ module.exports = {
   callback: (channel, tags, message, self, client) => {
     const storedMessagesArr = [
       "Our Discord https://discord.gg/S3dXsyTs is now LIVE! Come join us and chat about anything from web dev to gaming!",
+      "!drop klassy3Drop",
       "Play the drop game by typing in the command !drop with an emote of your choice. Hit 100 and become VIP",
       "For 1500 channel points, you can bet with/against other watchers on whether Splashley will chew on a bad or good jellybean.",
       "Play the WordGame! Type !wordgame to start!",
       "Thank you so much for watching this stream <3 It means a lot to me to have you here.",
       "Need some positive words of encouragement? Type !positive",
+      "!drop splash86Hydrate ",
     ];
 
     if (tags.username === "splashley") {
