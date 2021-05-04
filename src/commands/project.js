a561df40e98e6c4fc06de8a124ba5c929bcbb81d
splashley <ashleyjlhynes@gmail.com>
2021-05-04 19:58:35 -0400

    project update

--- src/commands/project.js
+++ src/commands/project.js
@@ -1,9 +1,6 @@
 module.exports = {
   text: "!project",
   callback: (channel, tags, message, self, client) => {
-    client.say(
-      channel,
-      "I'm building out a landing page as a cover letter for a job I want to apply to. Using react.js!"
-    );
+    client.say(channel, "Currently figuring out my next project..");
   },
 };
