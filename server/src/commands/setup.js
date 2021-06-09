2031a9d4078c468423bbc2d9414e3f502d75eae8
splashley <ashleyjlhynes@gmail.com>
2021-06-09 15:11:17 -0400

    setup update

--- server/src/commands/setup.js
+++ server/src/commands/setup.js
@@ -3,7 +3,7 @@ module.exports = {
   callback: (channel, tags, message, self, client) => {
     client.say(
       channel,
-      "CPU: i5-3350P CPU @ 3.10GHz | GPU: NVIDIA GeForce GTX 1650 | RAM: 16GB | MIC: MAONO AU-A04 | KEYBOARD: Redragon K582 | MOUSE: Redragon M602 | WEBCAM: Logitech HD Pro Webcam C920"
+      "CPU: i5-3350P CPU @ 3.10GHz | GPU: NVIDIA GeForce GTX 1650 | RAM: 16GB | MIC: MAONO AU-A04 | KEYBOARD: Redragon K582 | MOUSE: Redragon M602 | CAM: SONY A6000 W/ SIGMA 16MM F/1.4 LENS"
     );
   },
 };
