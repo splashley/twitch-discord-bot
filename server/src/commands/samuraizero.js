bbb4dc6b1421036b7945752c0a18b58cacfffa86
splashley <ashleyjlhynes@gmail.com>
2021-06-26 17:27:29 -0400

    supporting 0neguy's game! YYEEHAWW

---
+++ server/src/commands/samuraizero.js
@@ -0,0 +1,9 @@
+module.exports = {
+  text: "!samuraizero",
+  callback: (channel, tags, message, self, client) => {
+    client.say(
+      channel,
+      "Samurai Zero is a 3rd person tactical hack and slash game set in the beautiful science fantasy world of Edo, inspired by the Feudal Japanese Samurai. Learn more about it here https://szero.dev/steam"
+    );
+  },
+};
