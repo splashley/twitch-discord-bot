6ca3350682064fac4e776c9e335d55d91f267c1d
splashley <ashleyjlhynes@gmail.com>
2021-06-26 18:13:56 -0400

    small change

--- server/src/commands/about.js
+++ server/src/commands/about.js
@@ -3,7 +3,7 @@ module.exports = {
   callback: (channel, tags, message, self, client) => {
     client.say(
       channel,
-      "My name is Ashley and I stream primarily web development. From time to time I'll stream some gaming and small studying sessions. I'm a proud cat mom of six. I work full-time and go to university part-time for Business Technology Management. More can be read in my About me section."
+      "Samurai Zero is a 3rd person tactical hack and slash game set in the beautiful science fantasy world of Edo, inspired by the Feudal Japanese Samurai. Learn more about it here https://szero.dev/steam"
     );
   },
 };
