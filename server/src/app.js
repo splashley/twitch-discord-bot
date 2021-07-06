fbb9c696362f05672b62926a67f25af7af5480da
splashley <ashleyjlhynes@gmail.com>
2021-07-06 19:03:55 -0400

    added twitch api

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,49 @@
 // Bot Information
 require("dotenv").config();
+const { ApiClient } = require('twitch');
+const { ClientCredentialsAuthProvider } = require('twitch-auth');
+const { ConnectionAdapter, DirectConnectionAdapter, EventSubListener } = require('twitch-eventsub');
+
+const clientId = process.env.TWITCH_CLIENTID;
+const clientSecret = process.env.TWITCH_CLIENTSECRET;
+
+const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
+const apiClient = new ApiClient({ authProvider });
+
+const notificationUri = process.env.notificationUri;
+
+class StaticAdapter extends ConnectionAdapter {
+  constructor(uri, port) {
+    super();
+    this.uri = uri;
+    this.port = port;
+  }
+
+  get connectUsingSsl()  {
+    return true;
+  }
+
+  async getListenerPort() {
+    return this.port;
+  }
+
+  async getExternalPort() {
+    return 443;
+  }
+
+  async getHostName() {
+    return this.uri;
+  }
+}
+
+const listenerFunction = async function() {
+  const listener = new EventSubListener(apiClient,
+    new StaticAdapter(notificationUri, serverPort),
+    signingSecret);
+    await listener.listen();
+}
+
+listenerFunction();
 
 const { QuickTwitchBot } = require("quick-chat-bot");
 const path = require("path");
