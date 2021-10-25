42c0a367444ae44821fc5c0b662e0c063cb23158
splashley <ashleyjlhynes@gmail.com>
2021-10-25 19:02:19 -0400

    updated clap message

--- server/src/commands/clap.js
+++ server/src/commands/clap.js
@@ -3,6 +3,6 @@ module.exports = {
   callback: (channel, tags, message, self, client) => {
     const parse = message.slice(1).split(" ");
     const user = parse.splice(1);
-    client.say(channel, `peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap`);
+    client.say(channel, `peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap ${user} peepoClap`);
   },
 };
